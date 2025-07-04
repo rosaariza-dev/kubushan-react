8ac8d16685f99e39bb4c529c71cbf7108d96769a
Rosa Ariza <techrouse2025@gmail.com>
2025-07-03 22:43:18 -0500

    feat(about): agregar diseño responsivo de la sección sobre nosotros
    - se agregó un nuevo componente About en /components/sections
    - se implementó el diseño responsivo de la sección Sobre Nosotros
    - se creó la carpeta /assets/images en /public para las imágenes de fondo
    - se organizaron las imágenes principales en /src/assets/images y las de fondo en /public/assets/images
    - se importaron las imágenes principales del About en /data/resources.js
    - se ajustaron los estilos tipográficos en index.css y se agregó la clase .encabezado
    - se actualizó App.jsx para integrar varios componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import About from './components/sections/About'
 import Hero from './components/sections/Hero'
 
 function App() {
 
   return (
+   <>
    <Hero /> 
+   <About />
+   </>
   )
 }
 
--- src/data/resources.js
+++ src/data/resources.js
@@ -3,12 +3,21 @@ import coffeeMedium from "../assets/images/coffee_1_medium.avif";
 import coffeeDesktop from "../assets/images/coffee_1.avif";
 import play from "../assets/icons/play.svg";
 import user from "../assets/images/user.png";
+import womanSmall from "../assets/images/woman_small.avif";
+import womanMedium from "../assets/images/woman_medium.avif";
+import womanDesktop from "../assets/images/woman.avif";
+
 export const images = {
   hero: {
     coffeeSmall,
     coffeeMedium,
     coffeeDesktop,
     user,
+  },
+  about: {
+    womanSmall,
+    womanMedium,
+    womanDesktop,
   },
 };
 
