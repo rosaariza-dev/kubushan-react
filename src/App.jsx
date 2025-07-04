c82a8cd1cf51f8102e04e2ddad71826c176ed277
Rosa Ariza <techrouse2025@gmail.com>
2025-07-04 11:55:12 -0500

    feat(services): crear componente Services y diseño inicial
    - se creó el componente Services en /components/sections
    - se implementó el diseño inicial de la sección de servicios
    - se añadió el componente en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 import './App.css'
 import About from './components/sections/About'
 import Hero from './components/sections/Hero'
+import Services from './components/sections/Services'
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
    <>
    <Hero /> 
    <About />
+   <Services />
    </>
   )
 }
---
+++ src/components/sections/Services.jsx
@@ -0,0 +1,12 @@
+
+const Services = () => {
+  return (
+    <section id='services' className="py-20 px-8 md:px-16 flex flex-col items-center bg-linear-to-r from-custom-gray-gradient to-white ">
+        <span className='encabezado'>servicios</span>
+        <span className='title'>Nuestros servicios</span><br />
+        <span className="font-secondary block text-xl py-5 text-custom-black">En Kubushan pensamos en todo</span>
+    </section>
+  )
+}
+
+export default Services
